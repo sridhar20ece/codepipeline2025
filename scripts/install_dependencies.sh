07f0503a8f0b0b263476400fd6cb398468b31f43
sridhar20ece <50872557+sridhar20ece@users.noreply.github.com>
2025-06-22 22:03:28 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-aws s3 cp s3://mycodedeploy2025/app.jar /home/ubuntu/ 
-sudo chmod 777 /home/ubuntu/app.jar
 
-#sudo apt-get update
-#sudo apt-get install openjdk-11-jdk unzip -y
+sudo apt-get update
+sudo apt-get install openjdk-11-jdk unzip -y
 
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#3unzip awscliv2.zip
-#sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
  #make sure to change the bucket name here
+aws s3 cp s3://mycodedeploy2025/app.jar /home/ubuntu/ 
+sudo chmod 777 /home/ubuntu/app.jar
