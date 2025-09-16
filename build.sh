49211b27c037183de4246066eb3bfc9891145831
unknown <sridhar20ece@gmail.com>
2025-09-16 12:54:17 +0530

    Initial commit: build script-06

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Hello from Jenkins Build! Triggered on New $(date)"
-echo "Welcome to Devops 0000847t42dff4455777823bv25441t50-Build Test"
+echo "Welcome to Devops 0000847t42dff4455777823bv2544122t50-Build Test"
